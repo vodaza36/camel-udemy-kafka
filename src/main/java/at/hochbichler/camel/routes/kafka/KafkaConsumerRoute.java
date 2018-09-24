ada1f8351e8e6ed01c306e7ef4663822757d0ad2
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-24 10:40:51 +0200

    change route

--- src/main/java/at/hochbichler/camel/routes/kafka/KafkaConsumerRoute.java
+++ src/main/java/at/hochbichler/camel/routes/kafka/KafkaConsumerRoute.java
@@ -4,7 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class KafkaConsumerRoute extends RouteBuilder {
     public void configure() throws Exception {
-        from("kafka:127.0.0.1:9092?topic=my-topic&groupId=group1&consumersCount=1&autoOffsetReset=latest")
+        from("kafka:localhost:9092?topic=my-topic&groupId=group1&consumersCount=1&autoOffsetReset=latest")
                 .log("${body}")
                 .to("direct:readFromKafka");
     }
